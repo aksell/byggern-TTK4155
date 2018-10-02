14db8b53c54ea8d81c1804a1ec8610e6664599ad
TheOneEddy <edvard.grodem@gmail.com>
2018-10-02 10:59:22 +0200

    Add description to function

--- ByggernAtmega162/ByggernAtmega162/external_mem.h
+++ ByggernAtmega162/ByggernAtmega162/external_mem.h
@@ -1,6 +1,6 @@
 /*
  * external_mem.h
- *
+ * 
  * Created: 05.09.2018 10:15:43
  *  Author: akselsl
  */ 
@@ -15,6 +15,7 @@
 #define OLED_DATA_OFFSET 0x1200
 #define ADC_OFFSET 0x1400
 #define SRAM_OFFSET 0x1800
+
 
 void external_mem_init();
 void external_mem_test(void);
