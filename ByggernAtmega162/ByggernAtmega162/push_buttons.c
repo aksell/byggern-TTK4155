d7d4576ab6cdbbc0b8f0ef20968f593c9bb24a69
TheOneEddy <edvard.grodem@gmail.com>
2018-10-03 10:36:05 +0200

    Fixed floating pin bug

--- ByggernAtmega162/ByggernAtmega162/push_buttons.c
+++ ByggernAtmega162/ByggernAtmega162/push_buttons.c
@@ -23,15 +23,15 @@ void push_buttons_init() {
 	buttons[1].pin = PD5;
 	for(int i = 0; i < NUMB_BUTTONS; i++) {
 		DDRD &= ~(1 << buttons[i].pin);		// Set port to input
-		buttons[i].prev_state = ((1<< buttons[i].pin) & PINB) != 0;
+		buttons[i].prev_state = ((1<< buttons[i].pin) & PIND) != 0;
 	}
 }
 
 void push_buttons_poll() {
 	for(int i = 0; i < NUMB_BUTTONS; i++) {
-		bool pushed = ((1<< buttons[i].pin) & PINB) != 0;
+		bool pushed = ((1<< buttons[i].pin) & PIND) != 0;
 		if(pushed == 1 && buttons[i].prev_state == 0) {
-			buttons[i].state = 1;
+			buttons[i].state = true;
 		}
 		buttons[i].prev_state = pushed;
 	}
