f9b4095c36369a03cf916c757b0cd551d4949bad
Torbjørn Fuglestad <torbjfug@ntnu.no>
2018-10-17 19:38:26 +0200

    Improve detection of full buffer. Stil not working

--- ByggernAtmega162/ByggernAtmega162/CAN_buffer.h
+++ ByggernAtmega162/ByggernAtmega162/CAN_buffer.h
@@ -9,22 +9,31 @@
 #ifndef CAN_BUFFER_H_
 #define CAN_BUFFER_H_
 
-#define CAN_BUFFER_SIZE 88
+#define CAN_BUFFER_SIZE 30
 
 
 #include <avr/io.h>
+#include <util/atomic.h>
 
 #include "config.h"
 #include "utilities.h"
 #include "CAN_controller.h"
+#include "uart.h"
 
 
 
 void CAN_buffer_init();
+
+
 void CAN_buffer_write(can_message *message);
 can_message CAN_buffer_read();
+
+uint8_t CAN_buffer_remaining_size();
 bool CAN_buffer_is_empty();
+bool CAN_buffer_full();
+void CAN_buffer_set_full();
 
 void CAN_buffer_test();
+void CAN_buffer_test_2();
 
 #endif /* CAN_BUFFER_H_ */
