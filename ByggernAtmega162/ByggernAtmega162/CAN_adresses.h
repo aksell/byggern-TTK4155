c37a900d2e22d3e7a87a78cbb6177deee48e24c2
Torbjørn Fuglestad <torbjfug@ntnu.no>
2018-10-24 18:33:59 +0200

    Added file whit can addresses

---
+++ ByggernAtmega162/ByggernAtmega162/CAN_adresses.h
@@ -0,0 +1,20 @@
+/*
+ * CAN_adresses.h
+ *
+ * Created: 24.10.2018 15:19:56
+ *  Author: torbjfug
+ */ 
+
+
+#ifndef CAN_ADRESSES_H_
+#define CAN_ADRESSES_H_
+
+
+#define CAN_MOTOR 1
+#define CAN_IR 5
+#define CAN_JOYSTICK 10
+
+
+
+
+#endif /* CAN_ADRESSES_H_ */
