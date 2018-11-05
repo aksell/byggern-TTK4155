0f128bd676160ae6f77e4548af1dddd004b22a2b
Torbjørn Fuglestad <torbjfug@ntnu.no>
2018-11-05 18:05:17 +0100

    Added timer 0 for PI sample rate. Issues whith interaction whit other interrupts

--- ByggernAtmega162/ByggernAtmega2560/timer.h
+++ ByggernAtmega162/ByggernAtmega2560/timer.h
@@ -4,15 +4,17 @@
  * Created: 24.10.2018 09:25:06
  *  Author: EdvardOlaf
  */ 
-
+#define TIMER0_FREQUENZY 60
+#define PI_FREQUENZY TIMER0_FREQUENZY
 
 #ifndef TIMER_H_
 #define TIMER_H_
 #include "config.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
 #include <util/atomic.h>
+#include "dc_motor.h"
+
 void timer_init_fast_pwm_0();
 
 
@@ -20,5 +22,6 @@ void timer_init_fast_pwm_0();
 //Expects a value from 0 to 100
 void timer_fast_pwm_duty_cycle(uint16_t per_cent_duty);
 
-
+//Init 
+void timer0_init();
 #endif /* TIMER_H_ */
