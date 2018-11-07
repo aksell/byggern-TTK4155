2eea10f7a450649d21ec6c51d7eef0ad4e4937ce
TheOneEddy <edvard.grodem@gmail.com>
2018-11-07 21:13:39 +0100

    protocol change

--- ByggernAtmega162/ByggernAtmega162/CAN_protocol.h
+++ ByggernAtmega162/ByggernAtmega162/CAN_protocol.h
@@ -21,23 +21,21 @@
 #define CAN_START_GAME_ACK 4 //Acknowledge that node 2 has reacted to START_GAME
 
 //IO messages
-#define CAN_JOYSTICK_Y	10 //Send 1 signed byte from joystick Y with senter at 0
-#define CAN_JOYSTICK_X	11 //Send 1 signed byte from joystick X with senter at 0
+#define CAN_MOTOR_DELTA_POS	10 //Send 1 signed byte of desired delta position of main motor
 
-#define CAN_SLIDER_1 12 //Send 1 unsigned byte from Slider 1
-#define CAN_SLIDER_2 13 //Send 1 undigned byte from Slider 2
-
+#define CAN_SERVO_POS 12 //Send 1 unsigned byte to set servo position. 
 
 #define CAN_SOLANOIDE_TRIGGER_MS 14 //Request node 2 to activate solenoid together with two bytes of data indicating the ms of activation
 
 #define CAN_SOLANOIDE_TRIGGER_AND_HOLD 15 //Request node 2 to activate solenoid
 #define CAN_SOLANOIDE_RELIASE 16 //Request node 2 to reliase solenoid
 
+#define CAN_BALL_SENSOR_TRIGGERED 17 //Node 2 has detected ball
 
 //Music messages
 #define CAN_START_MUSIC 20 //Request a start of music in node 2 with value of 1 byte of music_t from "music.h"
-	
-#define CAN_STOP_MUSIC 21 //Stops music on node 2
+#define CAN_START_MUSIC_LOOP 21	 //Request a start of music played in loop in node 2 with value of 1 byte of music_t from "music.h"
+#define CAN_STOP_MUSIC 22 //Stops music on node 2
 
 
 #endif /* CAN_ADRESSES_H_ */
