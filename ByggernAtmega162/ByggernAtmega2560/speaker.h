a6d08e7ef0bd2094cce4d050310f757f85338646
TheOneEddy <edvard.grodem@gmail.com>
2018-11-07 17:58:05 +0100

    Change name to speker

---
+++ ByggernAtmega162/ByggernAtmega2560/speaker.h
@@ -0,0 +1,22 @@
+/*
+ * speaker.h
+ *
+ * Created: 05.11.2018 11:47:10
+ *  Author: EdvardOlaf
+ */ 
+
+
+#ifndef SPEAKER_H_
+#define SPEAKER_H_
+
+#include "avr/io.h"
+#include <util/atomic.h>
+#include "config.h"
+
+void speaker_init();
+
+void speaker_set_hz(uint16_t hz);
+void speaker_off();
+void speaker_on();
+uint16_t hz_to_timer_top(uint16_t hz, uint16_t prescalar);
+#endif /* speaker_H_ */
