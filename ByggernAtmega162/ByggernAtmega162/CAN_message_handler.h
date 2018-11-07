deb181af191198b647fb930b5e7f7d1b3ec17676
Torbjørn Fuglestad <torbjfug@ntnu.no>
2018-11-07 22:33:49 +0100

    "Removed obsolete functions"

--- ByggernAtmega162/ByggernAtmega162/CAN_message_handler.h
+++ ByggernAtmega162/ByggernAtmega162/CAN_message_handler.h
@@ -10,13 +10,14 @@
 #define CAN_MESSAGE_HANDLER_H_
 #include "config.h"
 #include "utilities.h"
-#include "CAN_adresses.h"
+#include "CAN_protocol.h"
 #include "CAN_buffer.h"
 #include "CAN_controller.h"
 #include "uart.h"
 #include "joystick.h"
 #include "slider.h"
 #include "ping_pong_loop.h"
+#include "push_buttons.h"
 
 void CAN_message_handler();
 
@@ -24,8 +25,12 @@ void CAN_message_handler();
 void CAN_joystick_X_transmit();
 void CAN_joystick_Y_transmit();
 
-
+//Transmits current slider value
 void CAN_slider_transmit();
+
+
+//Transmits current button state
+void CAN_push_button_transmit();
 
 
 #endif /* CAN_MESSAGE_HANDLER_H_ */
