eed84cb96dbb05ba450ea9f6ee5a3279b606900b
TheOneEddy <edvard.grodem@gmail.com>
2018-11-19 11:03:31 +0100

    Added motor controller message

--- ByggernAtmega162/ByggernAtmega162/CAN_protocol.h
+++ ByggernAtmega162/ByggernAtmega162/CAN_protocol.h
@@ -14,8 +14,8 @@
 
 
 //State messages
-#define CAN_GAME_OVER 2 //Message sendt to force other node out of game mode
-#define CAN_GAME_OVER_ACK 1 //Acknowledge that other node has changed out of game mode
+#define CAN_GAME_OVER 2 //Message sendt to force node 2 out of game mode
+#define CAN_GAME_OVER_ACK 1 //Acknowledge that node 2 has changed out of game mode
 
 #define CAN_START_GAME 4 //Message sent from node 1 indicating that a game is stating
 #define CAN_START_GAME_ACK 3 //Acknowledge that node 2 has reacted to START_GAME
@@ -23,12 +23,14 @@
 //IO messages
 #define CAN_MOTOR_POS	10 //Send 1 signed byte of desired position of main motor
 
+#define CAN_MOTOR_CALIBRATE 11 //Request node 2 to calibrate motor
+
 #define CAN_SERVO_POS 12 //Send 1 signed byte to set servo position. 
 
 #define CAN_SOLANOIDE_TRIGGER_MS 14 //Request node 2 to activate solenoid together with two bytes of data indicating the ms of activation
 
 #define CAN_SOLANOIDE_TRIGGER_AND_HOLD 15 //Request node 2 to activate solenoid
-#define CAN_SOLANOIDE_RELIASE 16 //Request node 2 to reliase solenoid
+#define CAN_SOLANOIDE_RELIASE 16 //Request node 2 to release solenoid
 
 #define CAN_BALL_SENSOR_TRIGGERED 17 //Node 2 has detected ball
 
